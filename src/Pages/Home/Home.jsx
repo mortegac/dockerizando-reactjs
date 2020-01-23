f63de6115a31c8026fd61246b7fc7b6ba9b7224a
Rodrigo Gonzalez <rodrigo.gonzalez.zura@gmail.com>
2020-01-22 22:50:39 -0300

    commit: se agrega el footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
             <div>
                 <Header/>
                 {/*<Body/>*/}
-                {/*<Footer/>*/}
+                <Footer/>
             </div>
         );
     }
